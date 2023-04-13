6bacb50eb0aee08fe2edeca3005574d709712db8
david <davidlab44@gmail.com>
2023-04-13 15:03:07 -0400

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,18 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () 
+{
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+
 Route::get('products', [App\Http\Controllers\ProductController::class, 'index']);
 Route::get('products/{id}', [App\Http\Controllers\ProductController::class, 'show']);
 Route::post('products',[App\Http\Controllers\ProductController::class, 'store']);
