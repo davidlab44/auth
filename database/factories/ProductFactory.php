7fac2396f707b97bf17a1e20c57b201ada030a13
david <davidlab44@gmail.com>
2023-04-13 10:00:49 -0400

    get request products created

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+	    'name' => Str::random(10),
+	    'description' => "Loren ipsum dolor tehjyt hepsum",
+	    'price' => 2000
+	];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
 	$this->call([
         	UserSeeder::class,
         	ManagerSeeder::class,
-        	ProductSeeder::class,
+        	ProductSeeder::class
         ]);
 
     }
