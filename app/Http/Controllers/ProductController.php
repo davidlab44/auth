debaaf2c39e4ec65a8a282094aabbf57de2d6a6c
david <davidlab44@gmail.com>
2023-04-13 09:35:01 -0400

    get-request

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,11 +1,15 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //  
+    public function index()
+    {
+        return Product::all();
+    }  
 }
 
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('products', [App\Http\Controllers\ProductController::class, 'index']);
 Route::get('posts', [App\Http\Controllers\PostController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
